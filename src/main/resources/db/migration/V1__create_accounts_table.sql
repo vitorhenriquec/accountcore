21827083028923c45af560ec9d0c0416421f92b3
Vitor Henrique <vitorhenrique908@gmail.com>
2025-10-18 09:41:20 -0300

    ACC-0001: Delete not necessary compose and sql file for migration of the accounts table

---
+++ src/main/resources/db/migration/V1__create_accounts_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE accounts (
+    id int8 NOT NULL,
+    document_number VARCHAR(255) NOT NULL,
+    active BOOLEAN NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    CONSTRAINT accounts_pk PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE accounts_seq_id INCREMENT BY 1
+START WITH 1
+MAXVALUE 99999999999
+MINVALUE 1
+CACHE 10;
