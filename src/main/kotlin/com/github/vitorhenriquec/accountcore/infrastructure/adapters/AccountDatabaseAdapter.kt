cfdbde70dfb07483708c713900403fe07acf0e4a
Vitor Henrique <vitorhenrique908@gmail.com>
2025-10-18 19:19:52 -0300

    ACC-0002: Add the interface for the AccountDatabaseAdapter

--- src/main/kotlin/com/github/vitorhenriquec/accountcore/infrastructure/adapters/AccountDatabaseAdapter.kt
+++ src/main/kotlin/com/github/vitorhenriquec/accountcore/infrastructure/adapters/AccountDatabaseAdapter.kt
@@ -1,5 +1,6 @@
 package com.github.vitorhenriquec.accountcore.infrastructure.adapters
 
+import com.github.vitorhenriquec.accountcore.domain.gateway.AccountDatabase
 import com.github.vitorhenriquec.accountcore.domain.model.AccountModel
 import com.github.vitorhenriquec.accountcore.infrastructure.repositories.AccountRepository
 import org.springframework.stereotype.Component
@@ -7,9 +8,9 @@ import org.springframework.stereotype.Component
 @Component
 class AccountDatabaseAdapter(
     private val repo: AccountRepository
-) {
+): AccountDatabase {
 
-    fun save(account: AccountModel): AccountModel {
+    override fun save(account: AccountModel): AccountModel {
         TODO("Not yet implemented")
     }
 
